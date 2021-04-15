403ce5caa886741807eac42d8a4af42e88a18b27
Hirasawa_Yui <YuiNozomi@vip.qq.com>
2021-04-15 17:35:01 +0800

    upload the db and readme.md

--- README.md
+++ README.md
@@ -2,13 +2,20 @@
  MyWorkSpace
 
  In this reporstory I saved my workspace folders.
- 
- ----------------
+
+----------------
  demo : This is the first demo Spring Boot project which order to know the every lines' meanings of pom.xml.
- 
- 
+
+
  rest-service : This is the restful service building demo,this project was built by the process of creating a “Hello, World” RESTful web service in Spring website.
  You can run the application from the command line with Gradle or Maven with rest-service-0.0.1-SNAPSHOT.jar
- 
- 
- accessing-data-mysql : This is the demo of building a Spring app to connected with MySQL.
+
+ accessing-data-mysql_v2 : This is the demo of building a Spring app to connected with MySQL.I failed to do this project at the first time,so it has v2 XD.
+
+echartsDemo : This is the demo of Echarts.js,I created a simple html file to using this mod.
+
+mybatis-plus-demo : This is the demo of using mybatis-plus to control a simple H2 database.
+
+fruit And springboot-vue-demo/fruit : This is the simple demo of SpringBoot + Axios + Vue + Mybatis-plus + ElementUI + MySQL,I using these structures to create a project which has add, delete, update functions, to use this project,you need to have a database named "db_example" and a MySQL database user named "mybatisuser" and password "ThePassword",I added this database's sql file into databases folder, you can run the db_database.sql to create the database for your computer,to boot this project you need to use `yarn install`and`npm run serve`in the springboot-vue-demo/fruit project as the front-end, and you should run the fruit project as the back-end,I thought you already make sure your MySQL and Java and other setting have been already install finished.
+
+in databases folder, you can see some of the projects' database .sql files
---
+++ databases/db_example.sql
@@ -0,0 +1,48 @@
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : MySQL
+ Source Server Type    : MySQL
+ Source Server Version : 80023
+ Source Host           : localhost:3306
+ Source Schema         : db_example
+
+ Target Server Type    : MySQL
+ Target Server Version : 80023
+ File Encoding         : 65001
+
+ Date: 15/04/2021 17:29:52
+*/
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for fruit
+-- ----------------------------
+DROP TABLE IF EXISTS `fruit`;
+CREATE TABLE `fruit`  (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(11) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `sale` int NULL DEFAULT NULL,
+  `icon` varchar(300) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 16 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of fruit
+-- ----------------------------
+INSERT INTO `fruit` VALUES (3, '橘子', 299, 'https://img0.baidu.com/it/u=1014450101,331943515&fm=26&fmt=auto&gp=0.jpg');
+INSERT INTO `fruit` VALUES (4, '葡萄', 555, 'https://img0.baidu.com/it/u=1014450101,331943515&fm=26&fmt=auto&gp=0.jpg');
+INSERT INTO `fruit` VALUES (5, '葡萄', 555, 'https://img0.baidu.com/it/u=1014450101,331943515&fm=26&fmt=auto&gp=0.jpg');
+INSERT INTO `fruit` VALUES (6, '火龙果', 133, 'http://pic30.nipic.com/20130605/5149126_102522221139_2.jpg');
+INSERT INTO `fruit` VALUES (7, '葡萄', 555, 'https://img0.baidu.com/it/u=1014450101,331943515&fm=26&fmt=auto&gp=0.jpg');
+INSERT INTO `fruit` VALUES (8, '葡萄', 555, 'https://img0.baidu.com/it/u=1014450101,331943515&fm=26&fmt=auto&gp=0.jpg');
+INSERT INTO `fruit` VALUES (9, '葡萄', 555, 'https://img0.baidu.com/it/u=1014450101,331943515&fm=26&fmt=auto&gp=0.jpg');
+INSERT INTO `fruit` VALUES (10, '葡萄', 555, 'https://img0.baidu.com/it/u=1014450101,331943515&fm=26&fmt=auto&gp=0.jpg');
+INSERT INTO `fruit` VALUES (11, '葡萄', 555, 'https://img0.baidu.com/it/u=1014450101,331943515&fm=26&fmt=auto&gp=0.jpg');
+INSERT INTO `fruit` VALUES (12, '葡萄', 555, 'https://img0.baidu.com/it/u=1014450101,331943515&fm=26&fmt=auto&gp=0.jpg');
+INSERT INTO `fruit` VALUES (15, '芒果', 222, 'https://img.zcool.cn/community/01cf305af39f4ea801216045607729.png@1280w_1l_2o_100sh.png');
+INSERT INTO `fruit` VALUES (16, '狗宝宝儿', 123, 'https://www.shianvip.com/wp-content/uploads/2020/12/1607880914.jpg');
+
+SET FOREIGN_KEY_CHECKS = 1;
