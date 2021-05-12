4067d995f8eb0776aedb76822bf342252743a993
Hirasawa_Yui <YuiNozomi@vip.qq.com>
2021-05-12 11:43:16 +0800

    trying to create Java file object
    still learning

--- springboot-vue-work/backend/src/main/java/backend/controller/QrcodedbController.java
+++ springboot-vue-work/backend/src/main/java/backend/controller/QrcodedbController.java
@@ -6,7 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.*;
 import java.util.List;
+import java.util.Objects;
 
 /**
  * <p>
@@ -53,25 +55,39 @@ public class QrcodedbController {
     }
 
     @PostMapping("/upload")
-    public boolean upload(@RequestParam("file")MultipartFile file){
+    public boolean upload(@RequestParam("file")MultipartFile file) throws IOException {
         // 由于前端发送的是 FormData 类型内容,所以这里用
         // RequestParam 注解获取内容
         if (!file.isEmpty()){
-            // 传输的内容名-->即 file
-            System.out.println(file.getName());
             // 文件大小
             System.out.println(file.getSize());
-            // 文件名
-            System.out.println(file.getOriginalFilename());
             // 提示文件获取成功
             System.out.println("Receive file well !");
             // 开始对获取到的文件进行操作
+            // 注意的是这里获取到的是 MultipartFile 对象,需要先转为 File 对象进行操作
+            // 首先创建一个 file 对象,用来当最终操作的对象
+            File f = null;
+            // 输出文件的新 name,即上传传输过来的文件名
+            System.out.println("getName : "+file.getName());
+            // 输出源文件的名称,即上传之前文件叫啥
+            System.out.println("originalName : "+file.getOriginalFilename());
+            // 开始创建文件,确保文件名不是空的
+            f = new File(Objects.requireNonNull(file.getOriginalFilename()));
+            try(InputStream in = file.getInputStream(); OutputStream os=new FileOutputStream(f)){
+                // 得到文件流.以文件流的方式输出到新文件
+                // 可以使用byte[] ss = file.getBytes();代替while循环
+                byte[] buffer = new byte[4096];
+                byte[] ss = file.getBytes();
+            }
+
             // 基本思路 : 将获取到的文件先存储给一个对象,之后对这个对象进行操作
             // 1. 给对象重命名,通过获取时间等内容命名新名称
             // 2. 将对象复制到最终要保存图片的路径中,然后以新名称命名文件
             // 3. 返回最终的图片路径
             // 最终要控制的就是 java 的 file 类使用,这个还得再看
             // 本项目只是个单表的小项目,要想使用企业类项目还得再找
+
+            // 设置将文件放在固定路径,并重新命名
             return true;
         }
         System.out.println("Can't receive the file.");
